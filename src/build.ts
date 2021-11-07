74c3078f35a0fb6211bbe98df7abc2d9ab78fcfc
zakizaki-ri9 <kic.yuuki@gmail.com>
2021-11-07 10:14:09 +0900

    github packages

--- src/build.ts
+++ src/build.ts
@@ -2,9 +2,9 @@
  * see. https://jp.vuejs.org/v2/cookbook/packaging-sfc-for-npm.html#%E3%83%91%E3%83%83%E3%82%B1%E3%83%BC%E3%82%B8%E5%8C%96%E3%81%95%E3%82%8C%E3%81%9F%E3%82%B3%E3%83%B3%E3%83%9D%E3%83%BC%E3%83%8D%E3%83%B3%E3%83%88%E3%81%AF%E3%81%A9%E3%81%AE%E3%82%88%E3%81%86%E3%81%AB%E3%81%AA%E3%81%A3%E3%81%9F%E3%81%8B
  */
 // @ts-nocheck
-import { CarouselItemList } from "@/components";
+import { CarouselItemList } from "./components";
 import { VueConstructor } from "vue/types/umd";
-import { Item } from "@/components/types";
+import { Item } from "./components/types";
 
 // Vue.use() によって実行される install 関数を定義
 export function install(vue: VueConstructor) {
